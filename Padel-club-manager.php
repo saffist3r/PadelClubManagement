8d56ab4cbc1622f41c3297e6139b49729fa36d61
safwen <safwen.ladhari@unity3d.com>
2024-02-15 16:09:45 +0100

    First Commuit / Project Structure.

---
+++ Padel-club-manager.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Padel Club Manager
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: saffist3r
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
